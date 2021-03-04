97043d30c96f5de63e4746feae0bb3f0121fc2be
qeelf <s.zosim@yahoo.com>
2021-03-04 23:19:37 +0400

    #4: add linter and fix style

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -6,10 +6,8 @@ import main.java.hexlet.code.Cli;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(final String[] args) {
         Cli.greeting();
     }
 }
