6b91bdb640e8953245b35bb4b02f4a399803d2e1
qeelf <s.zosim@yahoo.com>
2021-03-07 21:42:14 +0400

    #5: even game

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,13 +1,44 @@
 package hexlet.code;
 
 import main.java.hexlet.code.Cli;
+import main.java.hexlet.code.Even;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
 public class App {
+
+    private static final int GREETING_CODE_GAME = 1;
+    private static final int EVEN_CODE_GAME = 2;
+
     public static void main(final String[] args) {
-        Cli.greeting();
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.print("1 - Greet\n"
+                + "2 - Even\n"
+                + "0 - Exit\n"
+                + "Your choice: ");
+        Scanner sc = new Scanner(System.in);
+        int gameCode = 0;
+        try {
+            gameCode = sc.nextInt();
+        } catch (InputMismatchException exception) {
+            //do nothing
+        }
+        switch (gameCode) {
+            case GREETING_CODE_GAME:
+                Cli.greeting();
+                break;
+            case EVEN_CODE_GAME:
+                String name = Cli.greeting();
+                Even.playGame(name);
+                break;
+            default:
+                //do nothing
+        }
+
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -3,11 +3,12 @@ package main.java.hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
-    public static void greeting() {
+    public static String greeting() {
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         System.out.println("Hello, " + name + "!");
+        return name;
     }
 }
